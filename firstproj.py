96c52fbe760998faf26ae871946444a85defbebe
opilane <opilane@HP-CZC6297B89>
2021-10-28 15:13:12 +0300

    job well done

--- firstproj.py
+++ firstproj.py
@@ -1,3 +1,8 @@
-for x in range(100):
-    print("Hello world!")
-print("Bye world!")
+print("keep in mind you've got only 2 answers either good or bad")
+answer1=input(str("hello, how are you doing?"))
+if answer1=="good":
+    print("good for you buddy")
+elif answer1=="bad":
+    print("i hope it will get better buddy")
+else:
+    print ("you haven't answered properly program will shut down now")
