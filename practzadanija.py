d3998cfd11ca07179faa511211738661d0c7f285
arturLink <93324246+arturLink@users.noreply.github.com>
2021-11-09 16:40:58 +0200

    Add files via upload

---
+++ practzadanija.py
@@ -0,0 +1,38 @@
+#p=0
+#for i in range(15): #i=0,1,2,3...
+#    A=0
+#    while type(A)!=float:
+#        try:
+#            A=float(input("Sissesta"+str(i+1)+"arv"))
+#        except ValueError:
+#            print("Oi")
+#    if A==round(A):
+#        p+=1
+#        print(str(A)+" On täisarv")
+#    else:
+#        print(str(A)+" Ei ole täisarv")
+#print(str(p)+"Täisarvud")
+
+    #task7
+
+from random import *
+
+#A=randint(10,100)
+#B=randint(100,1000)
+#print("A= "+str(A))
+#print("B= "+str(B))
+#K=int(input("K: "))
+#for i in range(A,B+1):
+#    if i%K==0: print(i)
+
+    #task6
+#N=randint(1,10)
+#for i in range(N):
+#    Arv=int(input("Sissestage arv"))
+#    if Arv=0:
+#        p+=1
+#    elif Arv=1:
+#        n+=1
+#print("whatever1")
+#print("whatever2")
+#print("whatever3")
